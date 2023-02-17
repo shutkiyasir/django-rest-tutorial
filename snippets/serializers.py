534b57c01b2293207bf62202a8c6b929c7f9da4c
Yasir Mahabub <yasirmahabub@gmail.com>
2023-02-17 22:28:23 +0600

    Refactor serializers to use HyperlinkedModelSerializer

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -4,19 +4,32 @@ from rest_framework import serializers
 from snippets.models import Snippet
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.CharField(read_only=True, source="owner.username")
+    highlight = serializers.HyperlinkedIdentityField(
+        view_name="snippet-highlight", format="html"
+    )
 
     class Meta:
         model = Snippet
-        fields = ["id", "owner", "title", "code", "linenos", "language", "style"]
+        fields = [
+            "url",
+            "id",
+            "owner",
+            "title",
+            "code",
+            "linenos",
+            "language",
+            "style",
+            "highlight",
+        ]
 
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(
-        many=True, queryset=Snippet.objects.all()
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(
+        view_name="snippet-detail", many=True, queryset=Snippet.objects.all()
     )
 
     class Meta:
         model = User
-        fields = ["id", "username", "snippets"]
+        fields = ["url", "id", "username", "snippets"]
