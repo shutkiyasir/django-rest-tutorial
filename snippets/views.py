980f612dbcfc07300953f89e94e34874ad708a1a
Yasir Mahabub <yasirmahabub@gmail.com>
2023-02-17 11:50:08 +0600

    Add owner field in SnippetSerializer

--- snippets/views.py
+++ snippets/views.py
@@ -1,12 +1,16 @@
+from django.contrib.auth.models import User
+from rest_framework import generics
+
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
-from rest_framework import generics
-from django.contrib.auth.models import User
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
